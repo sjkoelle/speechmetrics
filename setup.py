a651743ea6ac2f3f505cbfb0e582e0bd92d1e768
Sam Koelle <sjkoelle@gmail.com>
2022-09-02 14:39:32 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
 	    # and there is a pip3 conflict if we have the following
 	    # line.
         #'gammatone @ git+https://github.com/detly/gammatone',
-        'pypesq @ git+https://github.com/vBaiCai/python-pesq',
+#         'pypesq @ git+https://github.com/vBaiCai/python-pesq',
         'srmrpy @ git+https://github.com/jfsantos/SRMRpy',
         'pesq @ git+https://github.com/ludlows/python-pesq',
     ],
